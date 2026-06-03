90eff0709f73c37a83c876250ec8f1349ecde6c5
A2SV <libenhailu04@gmail.com>
2026-06-03 20:28:39 +0300

    jun 3 daily challenge

---
+++ implementation/D_Lasers.py
@@ -0,0 +1,6 @@
+t = int(input())
+for _ in range(t):
+    n, m, x, y = map(int, input().split())
+    ns = list(map(int, input().split()))
+    ms = list(map(int, input().split()))
+    print(n + m)
